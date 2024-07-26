1769c63e80690f93a6405137574590294e83d9e7
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-07-26 18:16:55 +0500

    s

---
+++ DeadlineService.Domain/Models/Entity/PersonalInfo.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlineService.Domain.Models.Entity
+{
+    public class PersonalInfo
+    {
+        public PersonalInfo(int id,string email,string phoneNumber,int userId,string? description, byte[] photo=null)
+        {
+            
+        }
+        public int Id { get; set; }
+        public string? Description { get; set; }
+        public byte[]? Photo { get; set; }
+        public string Email { get; set; }
+        public string PhoneNumber { get; set; }
+        public DateTime CreateAt { get; set; }
+
+        public int UserId{ get; set; }
+        public User User{ get; set; }
+    }
+}
---
+++ DeadlineService.Domain/Models/Entity/User.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlineService.Domain.Models.Entity
+{
+    public class User
+    {
+        public User(int id, string username, string passwordHash)
+        {
+            Id = id;
+            Username = username;
+            PasswordHash = passwordHash;
+
+        }
+        public int Id{ get; set; }
+        public string Username { get; set; }
+        public string PasswordHash { get; set; }
+
+        public int? PersonalInfoId{ get; set; }
+        public PersonalInfo? PersonalInfo{ get; set; }
+        public List<Order>? Orders{ get; set; }
+
+
+    }
+}
