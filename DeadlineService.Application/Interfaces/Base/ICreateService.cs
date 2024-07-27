29528416bbde6294e2f0ae6f6fb3c8909502cc2d
Shokir <tursinbekovshokirxon@gmail.com>
2024-07-27 10:36:24 +0500

    добавлены базовые интерфейсы  CRUD и интерфейс IOrderService

---
+++ DeadlineService.Application/Interfaces/Base/ICreateService.cs
@@ -0,0 +1,7 @@
+﻿namespace DeadlineService.Application.Interfaces.Base
+{
+    public interface ICreateService<T> where T : class
+    {
+        public abstract Task<bool> DeleteAsync(T obj);
+    }
+}
---
+++ DeadlineService.Application/Interfaces/Services/IOrderService.cs
@@ -0,0 +1,18 @@
+﻿using DeadlineService.Application.Interfaces.Base;
+using DeadlineService.Domain.Models.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlineService.Application.Interfaces.Services
+{
+    public interface IOrderService:
+        ICreateService<Order>,
+        IGetAllService<Order>,
+        IUpdateService<Order>,
+        IDeleteService
+    {
+    }
+}
