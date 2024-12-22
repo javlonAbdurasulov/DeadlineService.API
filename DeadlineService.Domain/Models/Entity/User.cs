5610db817d686b67475829ff1d16072b19f3a5d5
Shokir <tursinbekovshokirxon@gmail.com>
2024-12-22 19:25:54 +0500

    asd

--- DeadlineService.Domain/Models/Entity/User.cs
+++ DeadlineService.Domain/Models/Entity/User.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace DeadlineService.Domain.Models.Entity
+﻿namespace DeadlineService.Domain.Models.Entity
 {
     public class User
     {
@@ -14,12 +8,12 @@ namespace DeadlineService.Domain.Models.Entity
             PasswordHash = passwordHash;
 
         }
-        public int Id{ get; set; }
+        public int Id { get; set; }
         public string Username { get; set; }
         public string PasswordHash { get; set; }
 
-        public int PersonalInfoId{ get; set; }
-        public PersonalInfo? PersonalInfo{ get; set; }
+        public int PersonalInfoId { get; set; }
+        public PersonalInfo? PersonalInfo { get; set; }
 
         public ICollection<Role>? Roles { get; set; }
         public ICollection<Order>? CreatedOrders { get; set; }
