08deec212b72e7bd265d460ddfc97c524f6605aa
abdurasulov2203 <abdurasulov2203@gmail.com>
2024-12-24 20:36:12 +0500

    empty interface IAuthorizationService

---
+++ DeadlineService.Application/Services/AuthorizationService.cs
@@ -0,0 +1,13 @@
+﻿using DeadlineService.Application.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeadlineService.Application.Services
+{
+    public class AuthorizationService:IAuthorizationService
+    {
+    }
+}
